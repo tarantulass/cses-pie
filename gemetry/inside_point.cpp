aaef37e86fb30c2e56b1bb84f5966fd8c17a967a
tarantulass <jeetgovind206@gmail.com>
2026-05-15 21:35:04 +0530

    completed the code for poitn inside by ray castign approach

--- gemetry/inside_point.cpp
+++ gemetry/inside_point.cpp
@@ -14,6 +14,7 @@ struct point{
 int main(){
 	//int n,m; less readable hence map n to sides and m to points
 	int sides, points, intersects;
+	bool bdry=false;
 	//  lets assume a ray horizontal then we have 2 points such that x,y and max x-> from sides,y no lets go more general
 	// here in the process of ray casting we check against each edge we dont check for one and if odd we proceed thats wrong
 	ll x,y;
@@ -23,18 +24,37 @@ int main(){
 	cin>>sides>>points;
 	for(int i=0;i<sides;i++){
 		cin>>x>>y;
-		sidecoords.push_back({x,y})
+		sidecoords.push_back({x,y});
 	}
 	for(int i=0;i<points;i++){
 		cin>>x>>y;
 		checkpoints.push_back({x,y});
 	}
 	// how do i check for edges since i only have vertices do i have to make edges??
+	// reading the question it clearly mentions that we have adjacent point for edges hence use of % again
 	for(auto &check: checkpoints){
-		for(auto& p: sidecoords){
-			if((check.y<)~=(check.y<p.)) && ())
+		for(int i=0;i<sides;i++){
+			// check for collinearity and inside
+			if(((check.y - sidecoords[i].y)*(sidecoords[(i+1)%sides].x - sidecoords[i].x)==(check.x-sidecoords[i].x)*(sidecoords[(i+1)%sides].y - sidecoords[i].y)) && (check.x<sidecoords[i].x)!=(check.x<sidecoords[(i+1)%sides].x)){
+				bdry = true;
+				cout<<"BOUNDARY";
+				break;
+			}
+
+			if((check.y<sidecoords[i].y)!=(check.y<sidecoords[(i+1)%sides].y)) && (check.x<sidecoords[i].x + ((check.y - sidecoords[i].y)/(sidecoords[(i+1)%sides].y - sidecoords[i].y))*(sidecoords[(i+1)%sides].x - sidecoords[i].x)))
 				intersects+=1;
+
+
+	// y inside and the x between the linear comibination
 		}
-		cout<<;
+		if(!bdry){
+			if(intersects%2){
+				cout<<"INSIDE";
+			}
+			else{
+				cout<<"OUTSIDE";
+			}
+
+		}
 	}
 }
