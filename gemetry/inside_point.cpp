78f0ff0e440fcfbac87bfd71a9b86e70742c5b42
tarantulass <jeetgovind206@gmail.com>
2026-05-15 23:44:19 +0530

    both checks required and indeed done

--- gemetry/inside_point.cpp
+++ gemetry/inside_point.cpp
@@ -1,6 +1,6 @@
- // the question once asked at an interview comes again
+// the question once asked at an interview comes again
+// interestingly we have this .push_back({x,y}) for a point strucutre
 #include<iostream>
-#include<cmath>
 #include<vector>
 using ll = long long;
 
@@ -33,26 +33,31 @@ int main(){
 	// how do i check for edges since i only have vertices do i have to make edges??
 	// reading the question it clearly mentions that we have adjacent point for edges hence use of % again
 	for(auto &check: checkpoints){
+		intersects = 0; // reinitialize again!!
+		bdry = false;
 		for(int i=0;i<sides;i++){
 			// check for collinearity and inside
-			if(((check.y - sidecoords[i].y)*(sidecoords[(i+1)%sides].x - sidecoords[i].x)==(check.x-sidecoords[i].x)*(sidecoords[(i+1)%sides].y - sidecoords[i].y)) && (check.x<sidecoords[i].x)!=(check.x<sidecoords[(i+1)%sides].x)){
+			if(((check.y - sidecoords[i].y)*(sidecoords[(i+1)%sides].x - sidecoords[i].x)==(check.x-sidecoords[i].x)*(sidecoords[(i+1)%sides].y - sidecoords[i].y)) && (check.x<=max(sidecoords[i].x,sidecoords[(i+1)%sides].x) && (check.x>=min(sidecoords[(i+1)%sides].x, sidecoords[i].x)) && (check.y>=min(sidecoords[(i+1)%sides].y, sidecoords[i].y)) && (check.y<=max(sidecoords[(i+1)%sides].y, sidecoords[i].y)))){
 				bdry = true;
-				cout<<"BOUNDARY";
+				cout<<"BOUNDARY\n";
 				break;
 			}
 
-			if((check.y<sidecoords[i].y)!=(check.y<sidecoords[(i+1)%sides].y)) && (check.x<sidecoords[i].x + ((check.y - sidecoords[i].y)/(sidecoords[(i+1)%sides].y - sidecoords[i].y))*(sidecoords[(i+1)%sides].x - sidecoords[i].x)))
+			// problem here is that normal division doesnt look good in ll hence we just cross multiply
+			// there is catch from normal cross multiplication here take care of that shit since y2-y1 may have different sign
+			ll dy = sidecoords[(i+1)%sides].y - sidecoords[i].y;
+			if(((check.y<sidecoords[i].y)!=(check.y<sidecoords[(i+1)%sides].y)) && (check.x < sidecoords[i].x + (double)(check.y - sidecoords[i].y) / dy * (sidecoords[(i+1)%sides].x - sidecoords[i].x))){
+
 				intersects+=1;
-
-
+			}
 	// y inside and the x between the linear comibination
 		}
 		if(!bdry){
 			if(intersects%2){
-				cout<<"INSIDE";
+				cout<<"INSIDE\n";
 			}
 			else{
-				cout<<"OUTSIDE";
+				cout<<"OUTSIDE\n";
 			}
 
 		}
