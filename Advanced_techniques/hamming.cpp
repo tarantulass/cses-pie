fbec914e9ee971a2dc2fb91f144c8f9c93b9545d
tarantulass <jeetgovind206@gmail.com>
2026-05-19 13:29:04 +0530

    the hamming file is giving errors of TLE clearly we need a better algorithm

---
+++ Advanced_techniques/hamming.cpp
@@ -0,0 +1,37 @@
+// finding the best pair again
+// but now we have bits only
+#include<bits/stdc++.h>
+using namespace std;
+
+// function return the hammign distance simple
+int xorstring(string& a, string& b){
+	int setbits=0;
+	for(int i=0;i<a.size();i++){
+		setbits+=(a[i]!=b[i]);
+	}
+	return setbits;
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("error_hamming.txt","w", stderr);
+#endif
+	// initialize
+	int testcases, length;
+	int minval = INT_MAX;
+	string s; // strings are given
+	vector<string> storestring;
+
+	// store input
+	cin>>testcases>>length;
+	for(int i=0;i<testcases;i++){
+		cin>>s;
+		storestring.push_back(s);
+	}
+	for(int i=0;i<testcases-1;i++){
+		for(int j=i+1;j<testcases;j++){
+			minval = min(minval,xorstring(storestring[i], storestring[j]));
+		}
+	}
+	cout<<minval;
+}
