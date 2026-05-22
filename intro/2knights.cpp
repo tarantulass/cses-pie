ac604b37d11cc0617a4ff867a753adeb1065fe55
tarantulass <jeetgovind206@gmail.com>
2026-05-22 16:08:39 +0530

    concept used on 2 knights listed out

--- intro/2knights.cpp
+++ intro/2knights.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 using namespace std;
+using ll=long long;
+/*
+Answers the puzzle how many mxn rectangle are there in a kxk grid?
+answer is (k-m-1)*(k-n-1)
+as we only need to have those number of startign indices just thatand there are so many possiblities
+*/
 
 int main(){
 	// initialize
-	int n,ans;
+	ll n,ans;
 	cin>>n;
 
 	// process
-	for(int i=1;i<n+1;i++){// to include n otherwise truncates to n-1
+	for(ll i=1;i<n+1;i++){// to include n otherwise truncates to n-1
 		ans = (i*i)*(i*i-1)/2 - 2*((i-1)*2*(i-2));
 		cout<<ans<<endl;
 	}
